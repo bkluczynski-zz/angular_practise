b9df6b5938ce46898f5516628576867c0b588cb0
Bart <kluczynskib@gmail.com>
2016-10-25 10:07:54 +0100

    testing the scope and inheritance

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,17 @@
-angular.module("todoListApp",[]);
+angular.module("todoListApp",[])
+
+.controller('mainCtrl', function($scope){
+  $scope.helloWorld = function(){
+    console.log('Hello there! This is the hello world controller function in the mainCtrl');
+  };
+})
+
+.controller('coolController', function($scope){
+  $scope.WhoAmI = function(){
+    console.log('This is not the main controller, it is cool Controller talkin');
+  }
+
+  $scope.helloWorld = function(){
+    console.log('This is the coolController hello world, inheritance is not gonna happen, cause i am defined here')
+  };
+});
